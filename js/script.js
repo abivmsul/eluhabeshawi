843a2988d32529cfc93f93376831d1ba3809902c
abivmsul <98397583+abivmsul@users.noreply.github.com>
2022-08-15 22:07:57 +0300

    responsive design 3 mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
+// Make mobile navigation work
 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-// const allLinks = document.querySelectorAll("a:link");
+ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
@@ -23,3 +30,30 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
